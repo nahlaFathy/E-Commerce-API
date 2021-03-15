40c9f0c990021dc3ae793c2b0083e4f8c79448a3
MoussaElenany95 <mousa.elenanyfciscu@gmail.com>
2021-03-15 19:41:37 +0200

    modify Access-Control-Allow-Headers to all

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "*");
   next();
 });
