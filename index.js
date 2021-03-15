4aa09c1bc2ddd9871eb99736ea8e8593bf4e5285
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-03-15 10:45:57 +0200

    header: Cors

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 
 app.use('/api/users', user);
 app.use('/api/admin', admin);
